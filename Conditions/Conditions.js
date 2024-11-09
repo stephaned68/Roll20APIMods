ae00c0321561549372f7bee0fbecc086fe69d011
Stéphane Devouard <sdevouard@gmail.com>
2024-11-09 14:07:33 +0100

    Conditions v1.2.0

--- Conditions/Conditions.js
+++ Conditions/Conditions.js
@@ -1,7 +1,7 @@
 /**
  * @name Conditions
  * @author stephaned68
- * @version 1.1.0
+ * @version 1.2.0
  * 
  * @description Set condition marker on token and display description in chat
  * 
@@ -9,12 +9,12 @@
  * This set of token markers must be installed in the Roll20 game
  * https://drive.google.com/drive/folders/1p8PTqBHkgSrKVqqOyI2_kFcPmbgLcX72 
  * 
- * A handout named as 'Condition:{name}' should be created for each condition
+ * A handout named as 'Condition:{name}' is created for each condition
  * Where {name} is the name of a condition token marker
  */
 on('ready', () => {
 
-  const MOD_VERSION = "1.1.0";
+  const MOD_VERSION = "1.2.0";
 
   const MOD_NAME = "MOD:Conditions"
 
@@ -47,7 +47,130 @@ on('ready', () => {
     "Pétrifié,Petrified"
   ].join("|")+"}";
 
-  const MARKER_BLOODIED = "Bleeding";
+  const CONDITIONS_HANDOUTS = [
+    {
+      name: "Blinded",
+      title: "Aveuglé-e",
+      effects: [
+        "Une créature aveuglée ne voit pas et rate automatiquement tout jet de caractéristique qui nécessite la vue.",
+        "Les jets d'attaque contre la créature ont un avantage, et les jets d'attaque de la créature ont un désavantage."
+      ]
+    },
+    {
+      name: "Charmed",
+      title: "Charmé-e",
+      effects: [
+        "Une créature charmée ne peut pas attaquer le charmeur ou le cibler avec des capacités ou des effets magiques nuisibles.",
+        "Le charmeur a un avantage à ses jets de caractéristique pour interagir socialement avec la créature.",
+      ]
+    },
+    {
+      name: "Deafened",
+      title: "Assourdi-e",
+      effects: [
+        "Une créature assourdie n'entend pas et rate automatiquement tout jet de caractéristique qui nécessite l’ouïe.",
+      ]
+    },
+    {
+      name: "Frightened",
+      title: "Effrayé-e",
+      effects: [
+        "Une créature effrayée a un désavantage aux jets de caractéristique et aux jets d'attaque tant que la source de sa peur est dans sa ligne de vue.",
+        "La créature ne peut se rapprocher volontairement de la source de sa peur.",
+      ]
+    },
+    {
+      name: "Grappled",
+      title: "Agrippé-e",
+      effects: [
+        "La vitesse d'une créature agrippée passe à 0, et elle ne peut bénéficier d'aucun bonus à sa vitesse.",
+        "L'état prend fin si la créature qui agrippe est incapable d'agir (voir l'état).",
+        "L'état se termine également si un effet met la créature agrippée hors de portée de la créature ou de l'effet qui l'agrippe, comme par exemple lorsqu'une créature est projetée par le sort vague tonnante.",
+      ]
+    },
+    {
+      name: "Incapacited",
+      title: "Neutralisé-e",
+      effects: [
+        "Une créature incapable d'agir ne peut effectuer aucune action ni aucune réaction.",
+      ]
+    },
+    {
+      name: "Invisible",
+      title: "Invisible",
+      effects: [
+        "Une créature invisible ne peut être vue sans l'aide de la magie ou un sens particulier. En ce qui concerne le fait de se cacher, la créature est considérée dans une zone à visibilité nulle. L'emplacement de la créature peut être détecté par un bruit qu'elle fait ou par les traces qu'elle laisse.",
+        "Les jets d'attaque contre la créature ont un désavantage, et les jets d'attaque de la créature ont un avantage.",
+      ]
+    },
+    {
+      name: "Paralyzed",
+      title: "Paralysé-e",
+      effects: [
+        "Une créature paralysée est incapable d'agir (voir l'état) et ne peut plus bouger ni parler.",
+        "La créature rate automatiquement ses jets de sauvegarde de Force et de Dextérité.",
+        "Les jets d'attaque contre la créature ont un avantage.",
+        "Toute attaque qui touche la créature est un coup critique si l'attaquant est à 1,50 mètre ou moins de la créature.",
+      ]
+    },
+    {
+      name: "Petrified",
+      title: "Pétrifié-e",
+      effects: [
+        "Une créature pétrifiée est transformée, ainsi que tout objet non magique qu'elle porte, en une substance inanimée solide (généralement en pierre). Son poids est multiplié par dix et son vieillissement cesse.",
+        "La créature est incapable d'agir (voir l'état), ne peut plus bouger ni parler, et n'est plus consciente de ce qui se passe autour d'elle.",
+        "Les jets d'attaque contre la créature ont un avantage.",
+        "La créature rate automatiquement ses jets de sauvegarde de Force et de Dextérité.",
+        "La créature obtient la résistance contre tous les types de dégâts.",
+        "La créature est immunisée contre le poison et la maladie, mais un poison ou une maladie déjà dans son organisme est seulement suspendu, pas neutralisé.",
+      ]
+    },
+    {
+      name: "Poisoned",
+      title: "Empoisonné-e",
+      effects: [
+        "Une créature empoisonnée a un désavantage aux jets d'attaque et aux jets de caractéristique.",
+      ]
+    },
+    {
+      name: "Prone",
+      title: "Au sol",
+      effects: [
+        "La seule option de mouvement possible pour une créature à terre est de ramper, à moins qu'elle ne se relève et mette alors un terme à son état.",
+        "La créature a un désavantage aux jets d'attaque.", 
+        "Un jet d'attaque contre la créature a un avantage si l'attaquant est à 1,50 mètre ou moins de la créature. Sinon, le jet d'attaque a un désavantage.",
+      ]
+    },
+    {
+      name: "Restrained",
+      title: "Entravé-e",
+      effects: [
+        "La vitesse d'une créature entravée passe à 0, et elle ne peut bénéficier d'aucun bonus à sa vitesse.",
+        "Les jets d'attaque contre la créature ont un avantage, et les jets d'attaque de la créature ont un désavantage.",
+        "La créature a un désavantage à ses jets de sauvegarde de Dextérité.",
+      ]
+    },
+    {
+      name: "Stunned",
+      title: "Etourdi-e",
+      effects: [
+        "Une créature étourdie est incapable d'agir (voir l'état), ne peut plus bouger et parle de manière hésitante.",
+        "La créature rate automatiquement ses jets de sauvegarde de Force et de Dextérité.",
+        "Les jets d'attaque contre la créature ont un avantage.",
+      ]
+    },
+    {
+      name: "Unconscious",
+      title: "Inconscient",
+      effects: [
+        "Une créature inconsciente est incapable d'agir (voir l'état), ne peut plus bouger ni parler, et n'est plus consciente de ce qui se passe autour d'elle.",
+        "La créature lâche ce qu'elle tenait et tombe à terre.",
+        "La créature rate automatiquement ses jets de sauvegarde de Force et de Dextérité.",
+        "Les jets d'attaque contre la créature ont un avantage.",
+        "Toute attaque qui touche la créature est un coup critique si l'attaquant est à 1,50 mètre ou moins de la créature.",
+      ]
+    },
+  ];
 
   const MACRO_ACTION = [
     `!token-mod --set statusmarkers|${CONDITION_QUERY}`,
@@ -73,7 +196,7 @@ on('ready', () => {
         { noarchive: true }
       );
     });
-  }
+  };
 
   /**
    * Detect new condition added to token
@@ -90,15 +213,14 @@ on('ready', () => {
       if (priorMarkers.includes(name)) return;
       displayCondition(name);
     });
-
-  }
+  };
 
   /**
    * Install MOD macro
    * @param {string} playerId - Roll20 player id
    * @returns {void}
    */
-  const installMOD = function (playerId) {
+  const installMOD = function(playerId) {
     let [ macro ] = findObjs({ _type: "macro", name: MACRO_NAME });
     if (!macro) {
       macro = createObj("macro", {
@@ -111,8 +233,38 @@ on('ready', () => {
       log(`Cannot create macro ${MACRO_NAME}`);
       return;
     }
-    macro.set("action", MACRO_ACTION);
-  }
+    macro.set("action", MACRO_ACTION);    
+  };
+
+  /**
+   * Create/Update Conditions handouts
+   */
+  const updateHandouts = function() {
+    CONDITIONS_HANDOUTS.forEach(handoutData => {
+      const name = `Condition:${handoutData.name}`;
+      let [ handout ] = findObjs({ _type: "handout", name });
+      if (!handout) {
+        handout = createObj("handout", {
+          name,
+          inplayerjournals: "all"
+        });
+      }
+      if (!handout) {
+        log(`Cannot create handout ${name}`);
+        return;
+      }
+      handout.get("notes", function(currNotes) {
+        const notes = `
+        <p style="font-family: 'proxima nova'"><strong>${ handoutData.title } [${ handoutData.name }]</strong></p>
+        <ul>
+        ${ handoutData.effects.map(effect => `<li>${ effect }</li>`).join("") }
+        </ul>
+        `;
+        if (notes !== currNotes) 
+          handout.set("notes", notes);
+      });
+    });
+  };
   
   /**
    * Wire-up change of token markers event
@@ -152,14 +304,15 @@ on('ready', () => {
    * Wire-up chat message event
    */
   on("chat:message", function (m) {
-    const chatMsg = m.content + " ";
     if (m.type === "api") {
-      if (chatMsg.includes("!condition ")) {
-        const name = chatMsg.split(" ")[1] || "";
-        if (name !== "") displayCondition(name);
+      const [ command, argument ] = m.content.split(" ");
+      if (command === "!condition ") {
+        if (argument) 
+          displayCondition(argument);
       }
       const hasTokenMod = typeof TokenMod === "object";
-      if (chatMsg.includes("!conditions ")) {
+      if (command === "!conditions") {
+        updateHandouts();
         if (hasTokenMod) {
           installMOD(m.playerid);
         } else {
@@ -174,5 +327,5 @@ on('ready', () => {
     }
   });
 
-  log(`${MOD_NAME} v${MOD_VERSION} running`);
+  log(`${MOD_NAME} v${MOD_VERSION} running - Use !conditions command to install or update handouts`);
 });
--- Conditions/README.md
+++ Conditions/README.md
@@ -4,7 +4,7 @@ Un script MOD pour Roll20 et D&D 5e permettant de gérer les conditions préjudi
 
 ## Version courante
 
-v1.0.0
+v1.2.0
 
 ## Dépendances
 
@@ -15,36 +15,40 @@ Token markers : https://drive.google.com/drive/folders/1p8PTqBHkgSrKVqqOyI2_kFcP
 # Mise en place
 - Vous devez installer le script MOD TokenMod
 - Vous devez télécharger le jeu de token markers indiqué ci-dessus et l'ajouter à la partie
-- Vous devez créer pour chaque condition un handout nommé <kbd>Condition:{nom}</kbd> qui contient le texte descriptif à afficher dans le chat
 
 ## Liste des noms de conditions
-- Grappled (agrippé)
-- Deafened (assourdi)
-- Prone (au sol)
-- Blinded (aveuglé)
-- Bloodied (en sang)
-- Charmed (charmé)
-- Frightened (effrayé)
-- Exhausted (épuisé)
-- Poisoned (empoisonné)
-- Restrained (entravé)
-- Stunned (étourdi)
-- Incapacited (inconscient)
-- Invisible (invisible)
-- Paralysed (paralysé)
-- Petrified (pétrifié)
+- Blinded (Aveuglé)
+- Bloodied (En sang)
+- Charmed (Charmé)
+- Deafened (Assourdi)
+- Exhausted (Epuisé)
+- Frightened (Effrayé)
+- Grappled (Agrippé)
+- Incapacited (Neutralisé)
+- Invisible (Invisible)
+- Paralysed (Paralysé)
+- Petrified (Pétrifié)
+- Poisoned (Empoisonné)
+- Prone (Au sol)
+- Restrained (Entravé)
+- Stunned (Etourdi)
+- Unconscious (Inconscient)
 
 # Utilisation
 
 <kbd>!condition {nom}</kbd> Affiche dans le chat la description de la condition nommée
 
-<kbd>!conditions</kbd> Crée la macro MOD-Conditions
+<kbd>!conditions</kbd> Crée la macro MOD-Conditions ainsi que les handouts descriptifs des conditions
 
 La macro MOD-Conditions permet de choisir une condition à appliquer au token sélectionné
 - TokenMod permet d'appliquer le token marker correspondant à la condition
 - La description trouvée dans le handout nommé Condition:{nom} est affichée dans un message de chat non archivé
 
 # Notes de version
+
+## v1.2.0 (2024-11-09)
+
+- Création ou Actualisation des handouts Condition:xxx
 
 ## v1.1.0 (2024-11-03)
 
